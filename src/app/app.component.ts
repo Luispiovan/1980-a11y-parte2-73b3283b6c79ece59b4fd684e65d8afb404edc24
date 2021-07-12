b3d6bb8aaff8278d3f84b32ffbe0227975ff150c
Grupo03 - LuisPiovan <ex150351@g.unicamp.br>
2021-07-12 17:03:13 -0300

    completion of implementation of accessibility components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 
-import { fade } from './shared/animations/fade';
-import { ModalRef } from './shared/components/modal/models/modal-ref';
-import { ModalService } from './shared/components/modal/services/modal.service';
+import {fade} from './shared/animations/fade';
+import {ModalRef} from './shared/components/modal/models/modal-ref';
+import {ModalService} from './shared/components/modal/services/modal.service';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -10,19 +11,35 @@ import { ModalService } from './shared/components/modal/services/modal.service';
   styleUrls: ['./app.component.scss'],
   animations: [fade]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   title = 'a11y-p2';
-  public firstName = 'Luis';
-  public modalRef: ModalRef;
+  // public firstName = 'Luis';
   public info = false;
+  public modalRef: ModalRef;
+  public form: FormGroup;
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.form= this.formBuilder.group({
+      firstName: ['Luis', Validators.required],
+      surName: ['Piovan', Validators.required],
+      age: ['30', Validators.required],
+      info: [false]
+    });
+  }
 
   public show(): void {
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details'
     });
+  }
+
+  public submit(): void {
+    console.log(this.form.value);
   }
 }
